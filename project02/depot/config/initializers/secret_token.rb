1f88fd95d36cd6ab857989eaec42645e15236193
Joe Martinez <joemarti@mines.edu>
2011-01-23 17:37:32 -0700

    Everything up to downloading the images pg 82 of rails

---
+++ project02/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b69bc35cac98e3c7beb120f1387bde3b9aba1d0acca96c5832240a1dcd5bc195af281b91f53d2bd8b9c1b02792312304ad4adc0ea3955c15b8088816574bc246'
