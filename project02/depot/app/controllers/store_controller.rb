53bce5c1bd714880c9cbf71078040079db4b4953
Joe Martinez <joemarti@mines.edu>
2011-01-31 19:06:06 -0700

    Done through page 101.

---
+++ project02/depot/app/controllers/store_controller.rb
@@ -0,0 +1,12 @@
+class StoreController < ApplicationController
+  def index
+	@products = Product.find_products_for_sale
+  end
+  
+  private
+  
+  def find_cart
+	session[:cart] ||= Cart.new
+  end
+
+end
