d204c34c70e7fca25cc7d1b859ceafecc9cf4173
Joe Martinez <joemarti@mines.edu>
2011-02-28 20:20:08 -0700

    Making progress changing erb files to haml

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+	validates_presence_of :firstname, :lastname, :username, :password
+	has_many :games
+
+	has_attached_file :photo, 
+					  :styles => { :small => ["72x72", :gif], :mini => ["24x24", :gif]},
+					  :url => '/assets/:class/:attachment/:id/:style/:filename',
+					  :default_url => '/images/picture.png'
+					  
+	cattr_reader :per_page
+	@@per_page = 10
+	
+	def to_s
+		username
+	end
+	
 end
