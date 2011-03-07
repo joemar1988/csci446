e0c3861c14e98c8fb091626b720b5e3e7bc40bfc
Joe Martinez <joemarti@mines.edu>
2011-03-06 20:53:08 -0700

    Declarative authorization setup

--- project06/app/controllers/application_controller.rb
+++ project06/app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class ApplicationController < ActionController::Base
   helper :all 
   protect_from_forgery 
   before_filer{|c|Authorization.current_user=c.current_user}
+  
+  protected
+  
+    def permission_denied
+      flash[error]="DENIED! You do not have permission to view this page."
+      redirect_to root_url
+    end
 
 end
